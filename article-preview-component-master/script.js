ba714d4bab872b31416df87e1b3cf8742d34bdb4
Sussana <118196337+Sussana7@users.noreply.github.com>
2025-06-12 14:55:43 +0000

    Delete article-preview-component-master directory

--- article-preview-component-master/script.js
+++
@@ -1,6 +0,0 @@
-const shareLink = document.querySelector(".share-link");
-const hiddenShare = document.querySelector(".hidden-share-links");
-
-shareLink.addEventListener("click", () => {
-    hiddenShare.classList.toggle("show");
-  });
